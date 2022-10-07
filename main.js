45c57274589c0e5db5bc495c6964dac60b0db6b3
Mikkel Deis <mikkeldc99@gmail.com>
2022-10-07 18:44:24 +0200

    Changed css code for tasklist

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-let tasks = document.querySelector(".content");
-console.log(tasks)
+let tasks = document.querySelector("#tasks");
+let form = document.querySelector("#new-todo-form");
+let input = document.querySelector("#new-todo-input");
+
+tasks.addEventListener
+let addButtons = function() {
+    let delButton = document.createElement("button");
+    delButton = delButton.classList.add("buttons");
+    delButton.innerText("Delete")
+    tasks.appendChild(delButton);
+}
+addButtons;
+
+
+let striked = false;
+content.addEventListener("dblclick", function(e){
+    if (striked === false ) {
+        e.toElement.classList = "strike"
+        striked = true;
+    }
+    else {
+        e.toElement.classList = "text"
+        striked = false;
+    }
+}
+)
